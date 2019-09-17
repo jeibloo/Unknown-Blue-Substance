1a570ce21342ef49c01c5075875ecea960d35961
Jason <jasonnova@protonmail.com>
2019-09-17 00:01:09 -0700

    Figuring out the layout

--- config.py
+++ config.py
@@ -1,1 +1,2 @@
 # All the config files needed (non-secret/no API keys etc)
+DEBUG = True # turns on debugging
---
+++ run.py
@@ -0,0 +1,3 @@
+# Run test server
+from app import app
+app.run(host='127.0.0.10', port=666, debug=True)
